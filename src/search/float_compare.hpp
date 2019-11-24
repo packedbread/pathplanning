67813e0a63fa6195d4ae52bcef62ba4da2e0a819
packedbread <ilya.ifanov@mail.ru>
2019-11-24 05:09:40 +0300

    Change used numerics & add tests

---
+++ src/search/float_compare.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <cmath>
+#include <type_traits>
+#include <limits>
+
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, T>::type* = nullptr>
+bool very_close_equals(T u, T v, T comparison_epsilon = std::numeric_limits<T>::epsilon()) {
+    return std::abs(u - v) < comparison_epsilon * std::abs(u) && std::abs(u - v) < comparison_epsilon * std::abs(v);
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, T>::type* = nullptr>
+bool very_close_less(T u, T v, T comparison_epsilon = std::numeric_limits<T>::epsilon()) {
+    return u - v < -comparison_epsilon * std::abs(u) && u - v < comparison_epsilon * std::abs(v);
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, T>::type* = nullptr>
+bool very_close_greater(T u, T v, T comparison_epsilon = std::numeric_limits<T>::epsilon()) {
+    return u - v > comparison_epsilon * std::abs(u) && u - v > comparison_epsilon * std::abs(v);
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, T>::type* = nullptr>
+bool close_enough_equals(T u, T v, T comparison_epsilon = std::numeric_limits<T>::epsilon()) {
+    return std::abs(u - v) < comparison_epsilon * std::abs(u) || std::abs(u - v) < comparison_epsilon * std::abs(v);
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, T>::type* = nullptr>
+bool close_enough_less(T u, T v, T comparison_epsilon = std::numeric_limits<T>::epsilon()) {
+    return u - v < -comparison_epsilon * std::abs(u) || u - v < comparison_epsilon * std::abs(v);
+}
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value, T>::type* = nullptr>
+bool close_enough_greater(T u, T v, T comparison_epsilon = std::numeric_limits<T>::epsilon()) {
+    return u - v > comparison_epsilon * std::abs(u) || u - v > comparison_epsilon * std::abs(v);
+}
