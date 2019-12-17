647d557dcc6ac34d9de5826e2c18ea519d1a6473
packedbread <42722576+packedbread@users.noreply.github.com>
2019-12-17 18:59:13 +0300

    Change I/O file interaction

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,21 @@
 using namespace planner;
 
 int main(int argc, char** argv) {
-    IOAdapter adapter = argc < 2 || std::string{ argv[1] } == "-" ? IOAdapter{ std::cin } : IOAdapter{ argv[1] };
+    IOAdapter adapter = argc < 2 ? IOAdapter{ std::cin } : IOAdapter{ argv[1] };
     auto map = adapter.read_map();
     auto locations = adapter.read_locations();
     auto search = adapter.read_algorithm();
     auto result = search->search(locations.first, locations.second, map);
     adapter.write_result(result, map);
-    if (argc < 3 || std::string{ argv[2] } == "-") {
+    if (argc < 2) {
         adapter.save_document(std::cout);
     } else {
-        adapter.save_document(argv[2]);
+        std::string input_filename{ argv[1] };
+        if (input_filename.size() >= 4 && input_filename.substr(input_filename.size() - 4) == std::string{ ".xml" }) {
+            adapter.save_document(input_filename.substr(0, input_filename.size() - 4) + "_log.xml");
+        } else {
+            adapter.save_document(std::cout);
+        }
     }
 
     return 0;
